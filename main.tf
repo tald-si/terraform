5e1d44421b86bc9e48133723edf011e69b75a507
Thaer AlDwaik <tald@siteimprove.com>
2023-12-21 14:16:47 -0800

    test

--- main.tf
+++ main.tf
@@ -1,11 +1,30 @@
 provider "aws" {
-  region = "us-east-1"  # Change to your desired AWS region
+  region = "eu-central-1"
+}
+
+terraform {
+  required_version = ">= 1.5.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.1"
+    }
+  }
+
+  cloud {
+    organization = "Thaer-org"
+
+    workspaces {
+      name = "bla-cli"
+    }
+  }
 }
 
 resource "aws_dynamodb_table" "example" {
-  name           = "test-table-tald"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "user_id"
+  name         = "test-table-tald-${terraform.workspace}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "user_id"
   attribute {
     name = "user_id"
     type = "S"
